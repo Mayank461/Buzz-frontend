09b0635025608d8ceba45051127d6c25ef74f1dc
kumarprashant24 <kumar.prashant@tothenew.com>
2022-04-05 15:19:33 +0530

    Updated Home Portion

--- src/App.js
+++ src/App.js
@@ -21,13 +21,13 @@ function App() {
       .catch((err) => console.log(err.message));
   }, []);
 
-  useEffect(() => {
-    axios
-      .get(`${API_URL}/auth/loginUser`)
-      .then((res) => res.data)
-      .then((user) =>  setUser(user))
-      .catch((err) => console.log(err.message));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get(`${API_URL}/auth/loginUser`)
+  //     .then((res) => res.data)
+  //     .then((user) =>  setUser(user))
+  //     .catch((err) => console.log(err.message));
+  // }, []);
 
   // const [user, setUser] = useState(false);
   const [emailLogin, setEmailLogin] = useState(false);
