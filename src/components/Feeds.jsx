a5908c49f9a99f6a6271d7b7f6a48cfcc68ed2b4
Mayank461 <mayank.jaggi@tothenew.com>
2022-04-08 20:19:38 +0530

    Fixed like and dislike

--- src/components/Feeds.jsx
+++ src/components/Feeds.jsx
@@ -1,115 +1,96 @@
 import axios from 'axios';
 import React, { useEffect, useReducer, useState } from 'react';
 import { API_URL } from '../config';
-import UserlistWidget from './UserlistWidget';
+import Post from './Post';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'increment':
-      return state + 1;
 
-    default:
-      return state;
-  }
-};
-export default function Feeds({ user, suggestFriend }) {
-  const [title, setTitle] = useState('');
-  const [image, setImage] = useState('');
-  const [url, setUrl] = useState('');
+export default function Feeds(user,{suggestFriend}) {
+  const [title, setTitle] = useState("")
+  const [image, setImage] = useState("")
+  const [url, setUrl] = useState("")
   const [userData, setUserData] = useState({});
   const [allUsers, setAllUsers] = useState([]);
   const [friendList, setFriendList] = useState([]);
+  const [refresh,setRefresh] = useState(0);
+  // const reverseArray = (user.user.posts).concat().reverse();
   const [posts, setPosts] = useState([]);
+  // const [check, setCheck] = useState(reverseArray)
 
-  const [todos, dispatch] = useReducer(reducer, 0);
 
-  useEffect(() => {
-    console.log('render');
-
-    loaduser();
-
-    // console.log(user.posts)
+  useEffect(() => {    
+    console.log(user);
+    loaduser();    
     axios
-      .get(`${API_URL}/user/getUser`, { withCredentials: true })
-      .then((res) => {
-        setAllUsers(res.data);
-      })
+      .get(`${API_URL}/posts/getPost`, { withCredentials: true })
+      .then((res) => { setPosts(res.data); })
       .catch((err) => console.log(err.message));
-  }, [todos]);
+  }, [refresh]);
 
   const loaduser = () => {
-    setUserData(user);
-    // const reverseArray = (user.posts).concat().reverse();
-    // setPosts(user?.posts?.concat().reverse());
+    setUserData(user.user);
 
-    setFriendList(user.friends.myFriends);
-  };
+    setFriendList(user.user.friends.myFriends);
+  }
   function logout() {
     axios
       .get(`${API_URL}/auth/logout`, { withCredentials: true })
-      .then((res) => console.log(res.data))
+      .then((res) => console.log(res.data[1]))
       .catch((err) => console.log(err.message));
   }
 
-  const postDetails = async () => {
-    // console.log(title)
-    // if(url === "" && title === "")
-    // {
-    //       alert("Sorry shaktimaan");
-    // }
-    console.log(posts);
-    const data = new FormData();
-    data.append('file', image);
-    data.append('upload_preset', 'buzz-app');
-    data.append('cloud_name', 'buzz-social-app');
-    await fetch(
-      'https://api.cloudinary.com/v1_1/buzz-social-app/image/upload',
-      {
-        method: 'post',
-        body: data,
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setUrl(data.url);
-        console.log(data);
+  const Inlike = (id) => {
+    axios
+      .post(`${API_URL}/posts/like`,{
+        post_id: id
+      }, { withCredentials: true })
+      .then((res) => setRefresh(refresh+1))
+      .catch((err) => console.log(err.message));      
+  }
+  const unlike = (id) => {
+    axios
+      .post(`${API_URL}/posts/unlike`,{
+        post_id: id
+      }, { withCredentials: true })
+      .then((res) => setRefresh(refresh+1))
+      .catch((err) => console.log(err.message));      
+  }
+
+  const postDetails = () => {
+
+    const data = new FormData()
+    data.append('file', image)
+    data.append('upload_preset', 'buzz-app')
+    data.append('cloud_name', 'buzz-social-app')
+    fetch('https://api.cloudinary.com/v1_1/buzz-social-app/image/upload', {
+      method: "post",
+      body: data
+    })
+      .then(res => res.json())
+      .then(data => {      
 
         fetch(`${API_URL}/posts/userPost`, {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json"
           },
           body: JSON.stringify({
             pic_url: data.url,
             caption: title,
-            user_id: userData._id,            
+            user_id: userData._id
           })
         })
+        .then(r => setRefresh(refresh+1))
+        alert("Your post uplaoded successfully")
+        // setCheck([...check, { post_url: url, post_caption: title }]);
+        setTitle('');
+        document.getElementById('file').value = "";
 
       })
       .catch(err => {
         console.log(err)
       })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    dispatch({ type: "increment" })
 
   }
-
-  const likepost = (e) => {
-    fetch(`${API_URL}/posts/like`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        userspost: userData._id,        
-      })
-    })    
-  }
-
   return (
     <>
       <div style={{ backgroundColor: '#F0F2F5' }}>
@@ -119,23 +100,13 @@ export default function Feeds({ user, suggestFriend }) {
             <div className="col-md-3 mt-3">
               <div className="card p-5 shadow-lg p-3 mb-5 bg-body rounded border-0">
                 <div className="d-flex justify-content-center">
-                  {'picture_url' in userData ? (
-                    <img
-                      src={userData.picture_url}
-                      className="card-img-top small-round-pic  round-img"
-                      alt="..."
-                    />
-                  ) : (
-                    <i className="fa-solid fa-user fa-2x card-img-top small-round-pic  round-img bg-warning d-flex justify-content-center align-items-center"></i>
-                  )}
+                  {('picture_url' in userData) ? <img src={userData.picture_url} className="card-img-top small-round-pic  round-img" alt="..." /> : <i className="fa-solid fa-user fa-2x card-img-top small-round-pic  round-img bg-warning d-flex justify-content-center align-items-center"></i>}
+
+
                 </div>
                 <div className="card-body">
-                  <h5 className="card-title text-center">
-                    {userData.firstname + ' ' + userData.lastname}
-                  </h5>
-                  <p className="card-text text-center">
-                    Newly Recruit at TTN {todos}{' '}
-                  </p>
+                  <h5 className="card-title text-center">{userData.firstname + " " + userData.lastname}</h5>
+                  <p className="card-text text-center">Newly Recruit at TTN </p>
                   <div className="d-flex justify-content-between mt-4">
                     <div>
                       <div className="text-center">234</div>
@@ -152,138 +123,46 @@ export default function Feeds({ user, suggestFriend }) {
             </div>
             {/* =======================================================================column 2nd ======================================================================== */}
             <div className="col-md-6  scroll-mid mt-3 ">
-              <div className="shadow p-3 mb-4 bg-body rounded">
-                <div className="d-flex align-items-center">
-                  <div className="">
-                    {'picture_url' in userData ? (
-                      <img
-                        src={userData.picture_url}
-                        className="card-img-top small-round-pic  round-img"
-                        alt="..."
-                      />
-                    ) : (
-                      <i className="fa-solid fa-user fa-2x card-img-top small-round-pic  round-img bg-warning d-flex justify-content-center align-items-center"></i>
-                    )}
+              <div className='shadow p-3 mb-4 bg-body rounded'>
 
-                    {/* <img
-                      src={userData.picture_url}
-                      className="card-img-top small-round-pic me-2  round-img"
-                      alt="..."
-                    /> */}
+                <div className='d-flex align-items-center'>
+                  <div className=''>
+                    {('picture_url' in userData) ? <img src={userData.picture_url} className="card-img-top small-round-pic  round-img" alt="..." /> : <i className="fa-solid fa-user fa-2x card-img-top small-round-pic  round-img bg-warning d-flex justify-content-center align-items-center"></i>}
+
                   </div>
-                  <div className="w-100">
+                  <div className='w-100'>
                     <input
                       type="text"
-                      className="caption p-2 rounded-pill form-control"
+                      className='caption p-2 rounded-pill form-control'
                       placeholder="Write Something in your mind"
                       value={title}
                       onChange={(e) => setTitle(e.target.value)}
                     />
                   </div>
+
                 </div>
-                <div className="text-center mt-2">
+                <div className='text-center mt-2'>
                   <input
-                    type="file"
-                    className=""
-                    onChange={(e) => setImage(e.target.files[0])}
-                  />
+                    type="file" className='' id="file" onChange={(e) => setImage(e.target.files[0])} />
                 </div>
-                <div className="text-center mt-2">
-                  <button
-                    className="btn btn-danger"
+                <div className='text-center mt-2'>
+                  <button className="btn btn-danger"
                     onClick={() => postDetails()}
-                  >
-                    Submit
+
+                  >Submit
                   </button>
+
                 </div>
               </div>
 
-              {posts.length === 0
-                ? ''
-                : posts.map((element, index) => {
-                    return (
-                      <div
-                        key={element._id}
-                        className="card p-3 mb-3 shadow p-3 mb-5 bg-body rounded border-0"
-                      >
-                        <div className="d-flex justify-content-between align-items-center mb-2">
-                          <div className="d-flex align-items-center">
-                            {'picture_url' in userData ? (
-                              <img
-                                src={userData.picture_url}
-                                className="card-img-top small-round-pic  round-img"
-                                alt="..."
-                              />
-                            ) : (
-                              <i className="fa-solid fa-user fa-2x card-img-top small-round-pic  round-img bg-warning d-flex justify-content-center align-items-center"></i>
-                            )}
+              {posts.map((element, index) => {
+                return (
+                  
+                 <Post index={index} data={element} inclike={Inlike} deslike={unlike}/>
+                )
+              }).reverse()}
 
-                            {/* <img
-                          src={userData.picture_url}
-                          className="card-img-top small-round-pic  round-img"
-                          alt="..."
-                        /> */}
 
-                            <div className="ms-2">
-                              {userData.firstname + ' ' + userData.lastname}
-                            </div>
-                          </div>
-
-                          <div className="d-flex align-items-center ms-2 round-img border rounded-circle icon-bg text-dark p-2">
-                            <i className="fa-solid fa-ellipsis"></i>
-                          </div>
-                        </div>
-                        <div className="ms-2 mb-2">{element.post_caption}</div>
-                        <img
-                          src={element.post_url}
-                          className="card-img-top rounded-3"
-                          alt="..."
-                        />
-
-                        <div className="d-flex justify-content-between mt-2">
-                          <div className="d-flex">
-                            <div>
-                              <i className="fa-solid fa-thumbs-up bg-primary round-img text-white p-1 me-2"></i>
-                              266
-                            </div>
-                            <div>
-                              <i className="fa-solid fa-heart bg-danger text-white round-img p-1 ms-2 me-2 "></i>
-                              306
-                            </div>
-                          </div>
-                          <div>1 commment</div>
-                        </div>
-
-                        <div className="d-flex justify-content-between mt-3 border-top border-bottom p-2">
-                          <div>
-                            <i className="fa-regular fa-thumbs-up me-2"></i>Like
-                          </div>
-                          <div>
-                            <i className="fa-regular fa-thumbs-down me-2"></i>
-                            Dislike
-                          </div>
-                          <div>
-                            <i className="fa-regular fa-message me-2"></i>
-                            Comment
-                          </div>
-                        </div>
-                        <div className="d-flex mt-3">
-                          <img
-                            src="https://images.unsplash.com/photo-1507438222021-237ff73669b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"
-                            className="card-img-top small-round-pic me-2  round-img"
-                            alt="..."
-                          />
-
-                          <input
-                            type="text"
-                            className="form-control rounded-pill"
-                            id=""
-                            placeholder="Write a comment..."
-                          />
-                        </div>
-                      </div>
-                    );
-                  })}
 
               <div className="card p-3 shadow p-3 mb-5 bg-body rounded border-0">
                 <div className="d-flex justify-content-between align-items-center mb-2">
@@ -347,11 +226,13 @@ export default function Feeds({ user, suggestFriend }) {
                 </div>
               </div>
             </div>
+
+
             {/* =============================================================================== column 3rd ================================================================================================== */}
             <div className="col-md-3 side-height mt-3">
               {/*========================================================================= Contacts ============================================================================== */}
 
-              <div className=" border p-2 scroll bg-white shadow-lg p-3 mb-3 bg-body rounded border-0">
+              <div className=" border p-2 scroll bg-white shadow-lg p-3 bg-body rounded border-0">
                 <div className="d-flex justify-content-between">
                   <div>Contacts</div>
                   <div>
@@ -361,38 +242,56 @@ export default function Feeds({ user, suggestFriend }) {
                   </div>
                 </div>
 
-                {friendList.length === 0 ? (
-                  <div className="text-center">You have no friends</div>
-                ) : (                  
-                  friendList.map(({firstname,lastname,picture_url}) => {
-                    return (
-                      <div className="d-flex">
+                {(friendList.length === 0) ? <div className='text-center'>You have no friends</div> :
+                  <div className="d-flex">
                     <div>
                       <img
-                        src={picture_url || require('../images/blank-profile.png')}
+                        src="https://images.unsplash.com/photo-1507438222021-237ff73669b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"
                         className="card-img-top small-round-pic me-2  round-img"
                         alt="..."
                       />
                     </div>
                     <div className="d-flex align-items-center">
-                      {firstname + " " + lastname}
+                      Prashant Mishra
                     </div>
                   </div>
-                    )
-                  })
-                )}
+                }
+
+
+
+
+
               </div>
-              <UserlistWidget
-                title="Friend Suggestions"
-                friendList={suggestFriend}
-              />
-              {/* ============================================================================ Suggestuons ================================================================================== */}
-            </div>
+              {/*============================================================================ Suggestuons ================================================================================== */}
+              <div className=" border p-2 scroll mt-3 bg-white shadow-lg p-3  bg-body rounded border-0">
+                <div className="d-flex justify-content-between">
+                  <div>Suggestions</div>
+                  <div>
+                    <div className="d-flex align-items-center ms-2 round-img border rounded-circle icon-bg  p-2">
+                      <i className="fa-solid fa-magnifying-glass-plus"></i>
+                    </div>
+                  </div>
+                </div>
 
-            {/* closing 3rd column  */}
-          </div>
+                <div className="d-flex">
+                  <div>
+                    <img
+                      src="https://images.unsplash.com/photo-1507438222021-237ff73669b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80"
+                      className="card-img-top small-round-pic me-2  round-img"
+                      alt="..."
+                    />
+                  </div>
+                  <div className="d-flex align-items-center">
+                    Prashant Mishra
+                  </div>
+                </div>
 
-          {/* Closing row  */}
+              </div>
+            </div> {/* closing 3rd column  */}
+
+
+          </div>  {/* Closing row  */}
+
         </div>
       </div>
     </>
