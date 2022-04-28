6f836da6af91d57fcc6f3fa313f29ac9ae5eae22
kumarprashant24 <kumar.prashant@tothenew.com>
2022-04-29 01:40:59 +0530

    fixing deployment error on heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export let API_URL;
 
 if (process.env.REACT_APP_STAGE === 'prod') {
-  API_URL = 'https://buzz-social-media.herokuapp.com';
+  API_URL = 'https://buzz-social-media.herokuapp.com/api';
 }
 if (process.env.REACT_APP_STAGE === 'dev') {
   API_URL = 'http://localhost:5000/api';
