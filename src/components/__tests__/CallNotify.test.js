18da1087d68979379bedd67b10a501f3ca02a9b6
mahir-ttn <mahir.asrani@tothenew.com>
2022-06-02 16:14:55 +0530

    fixed

--- src/components/__tests__/CallNotify.test.js
+++ src/components/__tests__/CallNotify.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import CallNotify from '../CallNotify';
 import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,19 +9,31 @@ jest.mock('socket.io-client');
 describe('Testing connection', () => {
   let socket = new SocketMock();
 
-  it('Should disply ~ New Calling Notification', async () => {
-    const user = {
-      _id: 1,
-      firstname: 'Test',
-      lastname: 'User',
-      friends: { myFriends: [], mySentRequests: [{}] },
-    };
-    const data = {
-      id: user?._id,
-      picture_url: user?.picture_url,
-      name: user.firstname + ' ' + user.lastname,
-    };
+  const user = {
+    _id: 1,
+    firstname: 'Test',
+    lastname: 'User',
+    friends: { myFriends: [], mySentRequests: [{}] },
+  };
+  const data = {
+    id: user?._id,
+    picture_url: user?.picture_url,
+    name: user.firstname + ' ' + user.lastname,
+  };
 
+  it('Should not disply ~ No notification', () => {
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          {' '}
+          <CallNotify user={user} socket={socket} />
+        </BrowserRouter>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Should display ~ New Calling Notification', async () => {
     render(
       <BrowserRouter>
         <CallNotify user={user} socket={socket} />
@@ -29,10 +41,11 @@ describe('Testing connection', () => {
     );
     socket.socketClient.emit('CallNotify', 123, data);
 
-    await new Promise((r) => setTimeout(r, 2000));
+    await new Promise((r) => setTimeout(r, 100));
 
     const name = screen.queryByTestId('name-field');
     expect(name).toBeTruthy();
+    expect(name).toHaveTextContent(data.name);
 
     const accept_btn = screen.queryByTestId('accept-btn');
     expect(accept_btn).toBeTruthy();
